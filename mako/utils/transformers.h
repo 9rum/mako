c90784411cb576d6cdf23f5ca61e804b2e20f05c
9rum <sanha.maeng@kakao.com>
2024-01-11 16:43:03 +0900

    utils: add definitions of constants and macros to interact with Hugging Face Hub Adapted from https://github.com/huggingface/huggingface_hub/blob/v0.20.0/src/huggingface_hub/constants.py

--- mako/utils/transformers.h
+++ mako/utils/transformers.h
@@ -14,17 +14,50 @@
 
 #pragma once
 
+#include <filesystem>
+#include <pwd.h>
 #include <string>
 #include <tuple>
+#include <unistd.h>
 #include <vector>
 
 #include <torch/torch.h>
 
 #include "mako/utils/export.h"
 
+namespace fs = std::filesystem;
+
+/// \brief Alternative to ``std::getenv``, equivalent to Python's ``os.getenv``.
+/// \param __key The name of environment variable.
+/// \param __default Default value returned if ``__key`` does not exist.
+/// \return Value of the environment variable ``__key``.
+inline std::string _getenv(const char *__key, std::string __default) {
+  const char *value = std::getenv(__key);
+  return value ? std::string(value) : __default;
+}
+
+// Definitions of constants and macros to interact with Hugging Face Hub.
+// Most of the below constants/macros are adapted from
+// https://github.com/huggingface/huggingface_hub/blob/v0.20.0/src/huggingface_hub/constants.py.
+fs::path _default_home             = fs::path(getpwuid(getuid())->pw_dir) / fs::path(".cache");
+std::string default_home           = _default_home.string();
+std::string _xdg_cache_home        = _getenv("XDG_CACHE_HOME", default_home);
+fs::path __hf_home                 = fs::path(_xdg_cache_home) / fs::path("huggingface");
+std::string _hf_home               = _getenv("HF_HOME", __hf_home.string());
+fs::path _default_cache_path       = fs::path(_hf_home) / fs::path("hub");
+std::string default_cache_path     = _default_cache_path.string();
+std::string _huggingface_hub_cache = _getenv("HUGGINGFACE_HUB_CACHE", default_cache_path);
+std::string _hf_hub_cache          = _getenv("HF_HUB_CACHE", _huggingface_hub_cache);
+std::string _default_revision      = "main";
+
+#define HF_HOME               _hf_home
+#define HUGGINGFACE_HUB_CACHE _huggingface_hub_cache
+#define HF_HUB_CACHE          _hf_hub_cache
+#define DEFAULT_REVISION      _default_revision
+
 namespace mako {
 namespace utils {
-/// \brief Utility to download and initialize HuggingFace's Transformers model.
+/// \brief Utility to download and initialize Hugging Face Transformers model.
 /// \param model_name_or_path A path to a directory containing model weights saved using ``save_pretrained``.
 /// \param cache_dir Path to the folder where cached files are stored.
 /// \param load_format Format of the model to load. Must be one of ``"auto"``, ``"safetensors"``, ``"pt"``, or ``"npcache"``.
@@ -33,9 +66,9 @@ namespace utils {
 /// \return Pairs of name and weight of the loaded model.
 std::vector<std::tuple<std::string, torch::Tensor>> MAKO_API hf_model_weights(
     std::string model_name_or_path,
-    std::string cache_dir   = "~/.cache/huggingface/hub",
+    std::string cache_dir   = HF_HUB_CACHE,
     std::string load_format = "auto",
-    std::string revision    = "main",
+    std::string revision    = DEFAULT_REVISION,
     bool fall_back_to_pt    = true);
 } // namespace utils
 } // namespace mako
