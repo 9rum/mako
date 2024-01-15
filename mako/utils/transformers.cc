5b8e57c11503ae2eb1a2a20de410d5ab2fbae45b
soiji <sooh0601@gmail.com>
2024-01-15 05:35:30 +0000

    utils: fix return value check of `std::string::compare`

--- mako/utils/transformers.cc
+++ mako/utils/transformers.cc
@@ -1,6 +1,6 @@
 // Adapted from https://github.com/vllm-project/vllm/blob/v0.2.7/vllm/model_executor/weight_utils.py
 // Copyright 2024 The Mako Authors
-// Copyright 2023 The vLLM team
+// Copyright 2023 The vLLM Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,19 +18,19 @@
 
 #include <algorithm>
 
-/// \brief Utility to find weight files from model directory
-/// \param model_name_or_path A path to a directory containing model weights saved using
+/// \brief Utility to find weight files from model directory.
+/// \param model_name_or_path A path to a directory containing model weights saved using ``save_pretrained``.
 /// \param cache_dir Path to the folder where cached files are stored.
 /// \param load_format Format of the model to load. Must be one of ``"auto"``, ``"safetensors"``, ``"pt"``, or ``"npcache"``.
 /// \param fall_back_to_pt If ``true``, will always allow pt format.
 /// \param revision An optional Git revision id which can be a branch name, a tag, or a commit hash.
-/// \return Pairs of name and weight of the loaded model.
-static std::tuple<std::string, std::vector<std::string>, bool> prepare_hf_model_weights(
+/// \return List of weight files.
+static inline std::tuple<std::string, std::vector<std::string>, bool> prepare_hf_model_weights(
         std::string model_name_or_path,
 	std::optional<std::string> cache_dir = std::nullopt,
         std::string load_format              = "auto",
         bool fall_back_to_pt                 = true,
-        std::optional<std::string> revision  = "None") {
+        std::optional<std::string> revision  = std::nullopt) {
     // check if model path exists
     auto is_local = fs::exists(fs::path(model_name_or_path));
 
@@ -45,14 +45,14 @@ static std::tuple<std::string, std::vector<std::string>, bool> prepare_hf_model_
         std::cerr << e.what();
     }
 
-    if (load_format.compare("auto")) {
+    if (load_format.compare("auto") == 0) {
         allow_patterns = {".safetensors", ".bin"};
-    } else if (load_format.compare("safetensors")) {
-        use_safetensors == true;
+    } else if (load_format.compare("safetensors") == 0) {
+        use_safetensors = true;
         allow_patterns = {".safetensors", ".bin"};
-    } else if (load_format.compare("pt")) {
+    } else if (load_format.compare("pt") == 0) {
         allow_patterns = {".pt"};
-    } else if (load_format.compare("npcache")) {
+    } else if (load_format.compare("npcache") == 0) {
         allow_patterns = {".bin"};
     }
 
@@ -62,15 +62,15 @@ static std::tuple<std::string, std::vector<std::string>, bool> prepare_hf_model_
 
     std::string hf_folder;
     if (!is_local) {
-        // TODO(soomin): implement snapshot_download
+        // TODO(soomin): implement ``snapshot_download``
     } else {
         hf_folder = model_name_or_path;
     }
 
     // load weight files path from model path directory
     std::vector<std::string> hf_weights_files = {};
-    for (auto const &pattern : allow_patterns) {
-        for (auto const &entry : fs::directory_iterator(hf_folder)) {
+    for (const auto &pattern : allow_patterns) {
+        for (const auto &entry : fs::directory_iterator(hf_folder)) {
             if (entry.path().extension().compare(pattern)) {
                 hf_weights_files.push_back(entry.path().string());
             }
@@ -82,7 +82,8 @@ static std::tuple<std::string, std::vector<std::string>, bool> prepare_hf_model_
             break;
         }
     }
-    // exclude files which is not used for loading weigths
+
+    // Exclude files that are not needed for inference.
     // https://github.com/huggingface/transformers/blob/v4.34.0/src/transformers/trainer.py#L227-L233
     if (!use_safetensors) {
 	std::vector<std::string> blacklist = {
@@ -92,7 +93,7 @@ static std::tuple<std::string, std::vector<std::string>, bool> prepare_hf_model_
             "scheduler.pt",
             "scaler.pt"
     	};
-	bool blacklisted = (std::find(blacklist.begin(), blacklist.end(), fs::path(entry).filename()) != blacklist.end());
+	bool blacklisted = std::find(blacklist.begin(), blacklist.end(), fs::path(entry).filename()) != blacklist.end();
         hf_weights_files.erase(std::remove_if(hf_weights_files.begin(), hf_weights_files.end(), blacklisted),
                 hf_weights_files.end());
     }
